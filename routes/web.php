7cd296614d3de9146247edc56ae6865d82519cf6
en6113 <mitayan3@gmail.com>
2026-09-07 14:30:43 +0900

    feat: 一般ユーザー用の申請一覧機能の追加

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ApplicationController;
 use App\Http\Controllers\AttendanceController;
 use App\Http\Controllers\AttendanceRecordController;
 use Illuminate\Support\Facades\Route;
@@ -9,15 +10,22 @@ Route::get('/', function () {
 });
 
 // ============================================================
-// 一般ユーザー用ルート(勤怠登録・勤怠一覧)
+// 一般ユーザー用ルート(勤怠登録・勤怠一覧・申請一覧)
 // ============================================================
 Route::middleware('auth:web')->group(function () {
+    // 勤怠登録
     Route::get('/attendance', [AttendanceRecordController::class, 'index'])->name('attendance-register');
     Route::post('/attendance', [AttendanceRecordController::class, 'store']);
 
+    // 勤怠一覧
     Route::get('/attendance/list', [AttendanceController::class, 'index'])->name('attendance.index');
     Route::get('/attendance/detail/{id}', [AttendanceController::class, 'show'])->name('attendance.show');
     Route::post('/attendance/detail/{id}', [AttendanceController::class, 'store']);
+
+    // 申請一覧
+    Route::get('/stamp_correction_request/list', [ApplicationController::class, 'index'])->name('application.index');
+    // 申請一覧の「詳細」リンクは勤怠詳細画面（AttendanceController::show）を再利用する
+    Route::get('/application/{id}', [AttendanceController::class, 'show']);
 });
 
 // ============================================================
